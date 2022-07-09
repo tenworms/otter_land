b2e86b50eb8a00b6324b84e68f05cf45071021de
Andre Pedro <andrerpedro170@gmail.com>
2022-07-09 15:00:29 -0600

    complete story 3

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/parks', to: 'parks#index'
   get '/parks/:id', to: 'parks#show'
+  get '/otters', to: 'otters#index'
 end
---
+++ spec/features/otters/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Otter Index' do
+  it 'displays all otters' do
+    otterland = Park.create!(title: 'Otter Land', hiring_workers: true, number_of_workers: 17)
+    mike = otterland.otters.create!(otter_age: 6, otter_name: "Mike", able_to_work: true)
+    alexis = otterland.otters.create!(otter_age: 14, otter_name: "Alexis", able_to_work: false)
+
+    visit "/otters"
+
+    expect(page).to have_content(mike.otter_age)
+    expect(page).to have_content(mike.otter_name)
+    expect(page).to have_content(mike.able_to_work)
+    expect(page).to have_content(alexis.otter_age)
+    expect(page).to have_content(alexis.otter_name)
+    expect(page).to have_content(alexis.able_to_work)
+
+  end
+end
