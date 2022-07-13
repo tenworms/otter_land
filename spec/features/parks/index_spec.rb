243d902130a0ad3147b2dec062286faac0e4ca2e
Andre Pedro <andrerpedro170@gmail.com>
2022-07-13 16:01:36 -0600

    complete user story 17 l

--- spec/features/parks/index_spec.rb
+++ spec/features/parks/index_spec.rb
@@ -38,4 +38,34 @@ RSpec.describe ' Park Index' do
   expect(otterkingdom.title).to appear_before(otterworld.title)
   end
 
+  # User Story 17, Parent Update From Parent Index Page
+    # As a visitor
+    # When I visit the parent index page
+    # Next to every parent, I see a link to edit that parent's info
+    # When I click the link
+    # I should be taken to that parents edit page where I can update its information just like in User Story 4
+
+    it 'has links to edit parks' do
+      otterland = Park.create!(title: 'Otter Land', hiring_workers: true, number_of_workers: 17)
+      otterkingdom = Park.create!(title: 'Otter Kingdom', hiring_workers: false, number_of_workers: 20)
+      visit "/parks"
+
+      within "#park-#{otterland.id}" do
+        expect(page).to have_content("#{otterland.title}")
+        expect(page).to have_link('Edit')
+      end
+
+      within "#park-#{otterkingdom.id}" do
+        expect(page).to have_content("#{otterkingdom.title}")
+        expect(page).to have_link('Edit')
+        click_link 'Edit'
+        expect(current_path).to eq("/parks/#{otterkingdom.id}/edit")
+      end
+
+    end
+
+    #<% @parks.each_with_index do |park, index|%>
+    #<div id="park_<%=index%>">
+    #  <h3>Title: <%= park.title %> | <a href="/parks/<%="#{park.id}"%>/edit">Edit Park</a> </h3>
+
 end
